96f312b0a07ba985e64e2d06384b11e09483dd06
lim_yungyoo <lim_yungyoo@elandsystems.com>
2017-11-17 10:52:11 +0900

    move App

---
+++ LEAF.VDSC.WEB/App/src/App.vue
@@ -0,0 +1,20 @@
+﻿<template>
+  <div id="app">    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+  font-size: 15px;
+}
+</style>
